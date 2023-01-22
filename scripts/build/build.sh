ab95f6bb4d8ec86f09751e466d7bd17abf2bcfd4
Ivan Gabriele <ivan.gabriele@gmail.com>
2023-01-23 00:49:32 +0100

    build(scripts): add build script with Prisma generations

--- scripts/build/build.sh
+++ scripts/build/build.sh
@@ -8,16 +8,12 @@ if [ -f "./.env" ]; then
   export $(egrep -v '^#' ./.env | xargs) > /dev/null
 fi
 
-if [ -z "$CI" ] && [ "${NODE_ENV}" != "production" ]; then
-  echo "Generating .env file..."
-  cp ./.env.example ./.env
-  yarn husky install
-else
+if [ "${NODE_ENV}" == "production" ]; then
   echo "Removing .env file..."
   rm -f ./.env
 fi
 
 echo "Generating Prisma runtime files..."
 yarn db:generate
-echo "Running database migration..."
-yarn db:migrate
+echo "Building Next.js app..."
+yarn next build
