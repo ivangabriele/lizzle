b9c7ab2a4ec4cd4d0fda1cfbe96e8ace75bcc4bd
Ivan Gabriele <ivan.gabriele@gmail.com>
2023-01-23 02:00:32 +0100

    build(scripts): remove .env loading in build script

--- scripts/build/build.sh
+++ scripts/build/build.sh
@@ -3,11 +3,6 @@
 # Exit when any command fails:
 set -e
 
-# Load .env file
-if [ -f "./.env" ]; then
-  export $(egrep -v '^#' ./.env | xargs) > /dev/null
-fi
-
 echo "Generating Prisma runtime files..."
 yarn db:generate
 echo "Building Next.js application..."
